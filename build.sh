fb6a905fbba6127eebd7b218a137f06ef31937a8
gabo <deobieta@gmail.com>
2016-03-10 14:56:51 -0600

    first commit

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env sh
 
-cd build/
 
-docker build -t aguamala/service-discovery:${BUILD_NUMBER} .
+CONSUL_TEMPLATE_VERSION=0.10.0
 
-DESTINATION=aguamala/haproxy-systemd:${BUILD_NUMBER}
+cd build-0-10-0/
+
+docker build -t aguamala/service-discovery:${CONSUL_TEMPLATE_VERSION} .
+
+DESTINATION=aguamala/haproxy-systemd:${CONSUL_TEMPLATE_VERSION}
 LATEST_DESTINATION=aguamala/service-discovery:latest
 
-docker tag aguamala/service-discovery:${BUILD_NUMBER} ${DESTINATION}
+docker tag aguamala/service-discovery:${CONSUL_TEMPLATE_VERSION} ${DESTINATION}
 docker push ${DESTINATION}
-docker tag -f aguamala/service-discovery:${BUILD_NUMBER} ${LATEST_DESTINATION}
+docker tag -f aguamala/service-discovery:${CONSUL_TEMPLATE_VERSION} ${LATEST_DESTINATION}
 docker push ${LATEST_DESTINATION}
